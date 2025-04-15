870c9d6cb17259b1fb0841aed31d73f1d04ea68a
Gagan Karnati <107645778+karnatigagan@users.noreply.github.com>
2025-04-15 10:31:37 -0400

    Update AITaskScheduler.java

--- src/AITaskScheduler.java
+++ src/AITaskScheduler.java
@@ -1,13 +1,16 @@
 /**
  * Enhanced interface for AI Task Scheduler.
  */
+//ok
 public interface AITaskScheduler extends AITaskSchedulerKernel {
 
     /**
      * Adjusts the priority of an existing task.
      *
-     * @param name the name of the task to update
-     * @param newPriority the new priority value
+     * @param name
+     *            the name of the task to update
+     * @param newPriority
+     *            the new priority value
      * @requires this.hasTask(name)
      * @ensures priority(name) = newPriority
      */
